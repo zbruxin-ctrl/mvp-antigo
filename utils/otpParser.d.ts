d01588a6ed15530c219e0c3c6beb1f6586c0cd3c
zbruxin-ctrl <zbruxin@gmail.com>
2026-05-14 23:17:40 -0300

    Delete utils directory

--- utils/otpParser.d.ts
+++
@@ -1,8 +0,0 @@
-import { MailMessage } from '../types/tempMail';
-export declare class OTPParser {
-    static extractOTP(text: string): string | null;
-    static extractFromMessageAsync(message: MailMessage): Promise<string | null>;
-    /** @deprecated use extractFromMessageAsync */
-    static extractFromMessage(message: MailMessage): string | null;
-}
-//# sourceMappingURL=otpParser.d.ts.map
